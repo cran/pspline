35c4ceffbec69e9b2a835e54a2a297455edb2a32
Brian Ripley <ripley@stats.ox.ac.uk>
2017-06-12 09:10:40 +0000

    version 1.0-18

---
+++ src/init.c
@@ -0,0 +1,19 @@
+#include <R_ext/RS.h>
+#include <stdlib.h> // for NULL
+#include <R_ext/Rdynload.h>
+
+/* .Fortran calls */
+extern void F77_NAME(pspline)(void *, void *, void *, void *, void *, void *, void *, void *, void *, void *, void *, void *, void *, void *, void *, void *, void *);
+extern void F77_NAME(splifit)(void *, void *, void *, void *, void *, void *, void *, void *, void *, void *, void *);
+
+static const R_FortranMethodDef FortranEntries[] = {
+    {"pspline", (DL_FUNC) &F77_NAME(pspline), 17},
+    {"splifit", (DL_FUNC) &F77_NAME(splifit), 11},
+    {NULL, NULL, 0}
+};
+
+void R_init_pspline(DllInfo *dll)
+{
+    R_registerRoutines(dll, NULL, NULL, FortranEntries, NULL);
+    R_useDynamicSymbols(dll, FALSE);
+}
